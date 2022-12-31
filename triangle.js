c8bc49f82e15bcdc6ffda50d0d92290bc535857c
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-31 14:05:05 +0530

    initial commit

---
+++ triangle.js
@@ -0,0 +1,5 @@
+const angleOne = document.querySelector("#first-angle");
+const angleTwo = document.querySelector("#second-angle");
+const angleThree = document.querySelector("#third-angle");
+const triangleBtn = document.querySelector("#triangle-btn")
+const angleMessage = document.querySelector("#showMessage");
