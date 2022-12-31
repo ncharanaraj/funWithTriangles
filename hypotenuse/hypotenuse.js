e0a7049aada7865567134878a625cbdfbdf39532
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-31 21:56:41 +0530

    functionality added

---
+++ hypotenuse/hypotenuse.js
@@ -0,0 +1,21 @@
+const adjacentA = document.querySelector("#adjacent");
+const oppositeO = document.querySelector("#opposite");
+const hypotenuseH = document.querySelector("#hypotenuse-btn");
+const hypotenuseOutput = document.querySelector("#hypotenuse-result");
+
+function squareInputs(adjacent, opposite){
+    const inputSquare =  Math.sqrt((adjacent*adjacent) + (opposite*opposite));
+    return inputSquare;
+}
+
+function caculateHypotenuse(){
+    const inputSquare = squareInputs(Number(adjacentA.value), Number(oppositeO.value))
+    
+    if(inputSquare > 0) {
+        hypotenuseOutput.innerText = "The length of hypotenuse is " + inputSquare;
+    } else {
+        hypotenuseOutput.innerText = "Please, fill all the fields."
+    }
+}
+
+hypotenuseH.addEventListener('click', caculateHypotenuse)
