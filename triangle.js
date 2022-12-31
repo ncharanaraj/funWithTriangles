f2a5b79a2b0caf7348d0807bf19ae93729a1c54e
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-31 15:05:56 +0530

    built a triangle logic

--- triangle.js
+++ triangle.js
@@ -2,4 +2,21 @@ const angleOne = document.querySelector("#first-angle");
 const angleTwo = document.querySelector("#second-angle");
 const angleThree = document.querySelector("#third-angle");
 const triangleBtn = document.querySelector("#triangle-btn")
-const angleMessage = document.querySelector("#showMessage");
+const triangleMessage = document.querySelector("#showMessage");
+
+function calculateSumOfAngles(angle1, angle2, angle3){
+    const sumOfAngles = angle1 + angle2 + angle3;
+    return sumOfAngles;
+}
+
+function isTriangle(){
+    const sumOfAngles = calculateSumOfAngles(Number(angleOne.value), Number(angleTwo.value), Number(angleThree.value))
+
+    if(sumOfAngles === 180){
+        triangleMessage.innerText = "Possible to make a triangle 🔺"
+    } else {
+        triangleMessage.innerText = "Not possible to make a triangle 🚫"
+    }
+}
+
+triangleBtn.addEventListener('click', isTriangle)
